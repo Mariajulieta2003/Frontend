0c9f626f8d00dad9efe9f70815c46a538a168bba
Julieta Rodriguez <104692472+Mariajulieta2003@users.noreply.github.com>
2025-11-14 18:11:45 -0300

    Delete src directory

--- src/shared/MessageDisplay.jsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import "./styles/MessageDisplay.css"; // o .module.css si lo usas
-
-export default function MessageDisplay({ message, type = "info" }) {
-  return <div className={`msg msg-${type}`}>{message}</div>;
-}
--- src/utils/money.js
+++
@@ -1,6 +0,0 @@
-export const formatARS = (n) =>
-  (n ?? 0).toLocaleString("es-AR", {
-    style: "currency",
-    currency: "ARS",
-    maximumFractionDigits: 0,
-  });
